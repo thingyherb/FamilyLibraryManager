3dacc3cb23e9b770c04dfedf158feecbac763816
Meng Yuan <849015644@qq.com>
2017-05-07 12:53:24 +0800

    合并王亮的代码

--- FamilyLibraryManager/app/src/main/java/familylibrarymanager/zhao/com/familylibrarymanager/ListFragment.java
+++ FamilyLibraryManager/app/src/main/java/familylibrarymanager/zhao/com/familylibrarymanager/ListFragment.java
@@ -1,22 +1,30 @@
 package familylibrarymanager.zhao.com.familylibrarymanager;
 
-import android.content.Context;
+import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.ListViewCompat;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import familylibrarymanager.zhao.com.familylibrarymanager.adapter.BookAdapter;
 import familylibrarymanager.zhao.com.familylibrarymanager.bean.Book;
+import familylibrarymanager.zhao.com.familylibrarymanager.constant.IntentConstant;
 import familylibrarymanager.zhao.com.familylibrarymanager.dao.LibraryDBDao;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
+ * {@link ListFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link ListFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -26,6 +34,10 @@ public class ListFragment extends Fragment {
     private LibraryDBDao mDao;
 
     private OnFragmentInteractionListener mListener;
+    private SwipeRefreshLayout refreshLayout;
+    private ListViewCompat list_books;
+    private BookAdapter bookAdapter;
+    private List<Book> data = new ArrayList<>();
 
     public ListFragment() {
         // Required empty public constructor
@@ -37,16 +49,19 @@ public class ListFragment extends Fragment {
      *
      * @return A new instance of fragment ListFragment.
      */
-    public static ListFragment newInstance() {
+    public static ListFragment newInstance(LibraryDBDao dao) {
         ListFragment fragment = new ListFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(IntentConstant.INTENT_DAO, dao);
+        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (mListener != null) {
-            mDao = mListener.getDao();
+        if (getArguments() != null) {
+            mDao = (LibraryDBDao) getArguments().getSerializable(IntentConstant.INTENT_DAO);
         }
     }
 
@@ -54,53 +69,54 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+        data.clear();
+        refreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.refreshLayout);
+        list_books = (ListViewCompat) view.findViewById(R.id.list_books);
+        List<Book> list = mDao.queryAllBookList();
+        if (list != null && list.size() > 0) {
+            data.addAll(list);
+        }
+        bookAdapter = new BookAdapter(getActivity(), data);
+        list_books.setAdapter(bookAdapter);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                data.clear();
+                data.addAll(mDao.queryAllBookList());
+                refreshLayout.setRefreshing(false);
+                bookAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        traverseData();
     }
-
-    //test 可删除
-    private void traverseData(){
-        List<Book> booklist = mDao.queryAllBookList();
-        Log.d(TAG, "size:"+(booklist==null?0:booklist.size()));
-        if(booklist!=null) {
-            for (Book book : booklist) {
-                printfBook(book);
-            }
-        }
-    }
-    private String TAG = "test";
-
-    //test 可删除
-    private void printfBook(Book book){
-        Log.d(TAG, "id:"+book.getId());
-        Log.d(TAG, "name:"+book.getBookname());
-        Log.d(TAG, "author:"+book.getAuthor());
-        Log.d(TAG, "borrower:"+book.getBorrower());
-        Log.d(TAG, "type:"+book.getType());
-        Log.d(TAG, "price:"+book.getPrice());
-        Log.d(TAG, "publicationdate:"+book.getPublicationDate());
-    }
-
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
-
 }
